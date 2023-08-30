6a76b5de46dcbda08285bff1b148895329272dc2
rayleigh <rayleigh190@gmail.com>
2023-08-30 20:09:51 +0900

    sol: 20230830 문제풀이

---
+++ baekjoon/solved.ad_CLASS/CLASS2/2775.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+  k = int(input())
+  n = int(input())
+  floor = [x for x in range(1, n+1)]
+  for i in range(k):
+    for j in range(1, n):
+      floor[j] += floor[j-1]
+  print(floor[n-1])
