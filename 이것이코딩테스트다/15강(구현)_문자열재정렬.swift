155b77c4d98c8754274733635d4235e93d424910
rayleigh <rayleigh190@gmail.com>
2024-05-18 23:05:17 +0900

    sol: 20240518 문제풀이

--- 이것이코딩테스트다/15강(구현)_문자열재정렬.swift
+++ 이것이코딩테스트다/15강(구현)_문자열재정렬.swift
@@ -1,6 +1,13 @@
-let input = readLine()!
-let letter = input.filter({$0.isLetter})
-let digit = input.filter({$0.isNumber}).map{$0.wholeNumberValue!}
+let input = Array(readLine()!)
+var result:[Character] = []
+var value = 0
 
-print(String(letter.sorted()), terminator: "")
-print(digit.reduce(0, +))
+for char in input {
+  if char.isLetter {
+      result.append(char)
+  } else {
+    value += char.wholeNumberValue!
+  }
+}
+
+print(String(result.sorted())+String(value))
