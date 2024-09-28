f5f6afecda10818978fe5975e8257bdb9848139b
rayleigh <rayleigh190@gmail.com>
2024-09-28 23:12:11 +0900

    resol: 프로그래머스 12911 재풀이

--- programmers/Lv2/12911. 다음 큰 숫자/다음 큰 숫자.swift
+++ programmers/Lv2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -1,23 +1,11 @@
 import Foundation
 
-func d2b(_ n:Int) -> String {
-    var remainders: String = ""
-    var quotient:Int = n
-    while quotient > 1 {
-        remainders += "\(quotient%2)"
-        quotient = quotient/2
-    }
-    remainders += "\(quotient)"
-    return remainders
-}
-
 func solution(_ n:Int) -> Int {
     var num = n
-    let numOfOne = d2b(num).filter({$0=="1"}).count
+    let numOfOne = num.nonzeroBitCount
     while true {
         num += 1
-        let nextNumOfOne = d2b(num).filter({$0=="1"}).count
-        if numOfOne == nextNumOfOne { break }
+        if numOfOne == num.nonzeroBitCount { break }
     }
     return num
 }
