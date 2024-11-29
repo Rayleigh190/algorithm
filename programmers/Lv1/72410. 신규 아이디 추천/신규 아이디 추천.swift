1aad9e52e7a9885c413dc04adaa0812e21f05fa4
rayleigh <rayleigh190@gmail.com>
2024-11-29 15:31:54 +0900

    sol: programmers 72410 solved

--- programmers/Lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
+++ programmers/Lv1/72410. 신규 아이디 추천/신규 아이디 추천.swift
@@ -1,60 +1,50 @@
 import Foundation
 
 func solution(_ new_id:String) -> String {
-    var result = new_id
+    var id = new_id
+    var newId = ""
     
     // 1단계 : new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-    result = result.lowercased()
+    id = id.lowercased()
     
     // 2단계 : new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-    do {
-        let patter = "[^a-z0-9\\-\\_\\.]"
-        let regex = try NSRegularExpression(pattern: patter)
-        let range = NSRange(result.startIndex..<result.endIndex, in: result)
-        result = regex.stringByReplacingMatches(in: result, range: range, withTemplate: "")
-    } catch {
-        print("2단계 오류")
+    for i in id {
+        if i.isLetter || i.isNumber || i == "-" || i == "_" || i == "." {
+            newId.append(i)
+        }
     }
     
     // 3단계 : new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
-    do {
-        let patter = "\\.{2,}"
-        let regex = try NSRegularExpression(pattern: patter)
-        let range = NSRange(result.startIndex..<result.endIndex, in: result)
-        result = regex.stringByReplacingMatches(in: result, range: range, withTemplate: ".")
-    } catch {
-        print("3단계 오류")
+    while newId.contains("..") {
+        newId = newId.replacingOccurrences(of: "..", with: ".")
     }
     
     // 4단계 : new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
-    do {
-        let patter = "^\\.|\\.$"
-        let regex = try NSRegularExpression(pattern: patter)
-        let range = NSRange(result.startIndex..<result.endIndex, in: result)
-        result = regex.stringByReplacingMatches(in: result, range: range, withTemplate: "")
-    } catch {
-        print("4단계 오류")
+    while newId.hasPrefix(".") {
+        newId.removeFirst()
+    }
+
+    while newId.hasSuffix(".") {
+        newId.removeLast()
     }
     
     // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-    if result.count == 0 {
-        result = "a"
+    if newId.count == 0 {
+        newId = "a"
     }
     
     // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다. 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-    if result.count >= 16 {
-        result = String(result[result.startIndex...result.index(result.startIndex, offsetBy: 14)])
+    if newId.count >= 16 {
+        newId = Array(newId.map{String($0)})[0..<15].joined()
     }
-    if result.last == "." {
-        result.removeLast()
+    if newId.hasSuffix(".") {
+        newId.removeLast()
     }
     
     // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-    if result.count <= 2 {
-        let lastChar = String(result.last!)
-        while result.count < 3 {
-            result = result + lastChar
-        }
+    while newId.count < 3 {
+        newId.append(newId.last!)
     }
-    return result
+    
+    return newId
 }
